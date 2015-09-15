1da7602472bddf3e118ab620e45c96c20050ecbd
AsmodeusXI <AsmodeusXI@gmail.com>
2015-09-14 21:16:45 -0500

    feat: adding CSS and Travis to worldshaper
    Include CSS and Travis
    Gulpify worldshaper
    Update gulp plugins
    Include the correct NPM plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
 var del = require('del');
 var Server = require('karma').Server;
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
     scripts: ['src/worldshaper.js', 'src/**/*module.js', 'src/**/*Ctrl.js', 'src/**/*Svc.js'],
     angular: 'node_modules/angular/**/*.min.js',
     tests: 'src/**/*Spec.js',
+    thirdparty: [
+        'node_modules/angular/angular.min.js'
+    ],
+    tests: 'src/**/*Spec.js',
+    less: 'src/**/*.less',
+    html: 'src/**/*.html',
     output: 'dist',
     gulp: 'gulpfile.js'
 }
@@ -20,21 +25,32 @@ gulp.task('clean:dist', function () {
 
 gulp.task('build:internal', function () {
     return gulp.src(paths.scripts)
-                .pipe(sourcemaps.init())
-                .pipe(concat('internal.js'))
-                .pipe(sourcemaps.write('maps'))
+                .pipe(plugins.jshint())
+                .pipe(plugins.jshint.reporter('jshint-stylish'))
+                .pipe(plugins.sourcemaps.init())
+                .pipe(plugins.uglify())
+                .pipe(plugins.concat('internal.min.js'))
+                .pipe(plugins.sourcemaps.write('maps'))
                 .pipe(gulp.dest(paths.output));
 });
 
 gulp.task('build:third-party', function () {
     return gulp.src(paths.angular)
-                .pipe(concat('third-party.js'))
+                .pipe(plugins.concat('third-party.js'))
                 .pipe(gulp.dest(paths.output));
+});
+
+gulp.task('build:less', function () {
+    return gulp.src(paths.less)
+                .pipe(plugins.less())
+                .pipe(gulp.dest(paths.output))
+                .pipe(plugins.livereload());
 });
 
 gulp.task('build', [
     'clean:dist',
     'build:internal',
+    'build:less',
     'build:third-party'
 ]);
 
@@ -49,6 +65,9 @@ gulp.task('default', ['build', 'tests:run']);
 
 gulp.task('listen', function () {
     gulp.start('default');
+    plugins.livereload.listen();
     gulp.watch(paths.scripts, ['default']);
+    gulp.watch(paths.less, ['build:less']);
+    gulp.watch(paths.html, ['build:less']);
     gulp.watch(paths.tests, ['tests:run']);
 });
